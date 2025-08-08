7605c55085e02899fffe3c6c11a99dc6218626c3
Samuel Silva Costa <samuel.silva.suporte@outlook.com>
2025-08-07 21:48:34 -0300

    Feat: Configuring AppDbContext

---
+++ Back-end/VaccinationCard/VaccinationCard.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccinationCard.Domain.Entities;
+using VaccinationCard.Infrastructure.Configurations;
+
+namespace VaccinationCard.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Vaccine> Vaccines { get; set; }
+
+        public DbSet<VaccineDoseType> VaccineDoseTypes { get; set; }
+
+        public DbSet<VaccinationRecord> VaccinationRecords { get; set; }
+
+        public DbSet<DoseType> DoseTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new VaccineDoseTypeEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new DoseTypeEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new PersonEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new VaccinationRecordEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new VaccineEntityConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseMySQL("server=localhost;uid=root;pwd=root;database=vaccine-db-dev");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
