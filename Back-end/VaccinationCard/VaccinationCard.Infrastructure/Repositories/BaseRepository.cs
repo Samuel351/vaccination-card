42eb214beb9a19dd3026fb779f35544d65689ffd
Samuel Silva Costa <samuel.silva.suporte@outlook.com>
2025-08-22 18:16:54 -0300

    Feat: Creating base repository

---
+++ Back-end/VaccinationCard/VaccinationCard.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccinationCard.Application.Interfaces.Repositories;
+using VaccinationCard.Domain.Entities;
+using VaccinationCard.Infrastructure.Data;
+
+namespace VaccinationCard.Infrastructure.Repositories
+{
+    internal class BaseRepository<T>(AppDbContext appDbContext) : IBaseRepository<T> where T : EntityBase
+    {
+
+        private readonly AppDbContext _appDbContext = appDbContext;
+        private readonly DbSet<T> _dbSet = appDbContext.Set<T>();
+
+        public async Task<T?> GetByIdAsync(Guid id) =>
+            await _dbSet.FirstOrDefaultAsync(e => e.EntityId == id);
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity is not null)
+            {
+                _dbSet.Remove(entity);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
+
