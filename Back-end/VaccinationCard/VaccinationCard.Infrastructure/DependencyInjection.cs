41976a218a46cb2fb883a7b4323352901b5d8753
Samuel Silva Costa <samuel.silva.suporte@outlook.com>
2025-08-22 18:23:41 -0300

    Update DependencyInjection.cs

--- Back-end/VaccinationCard/VaccinationCard.Infrastructure/DependencyInjection.cs
+++ Back-end/VaccinationCard/VaccinationCard.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VaccinationCard.Application.Interfaces.Repositories;
 using VaccinationCard.Infrastructure.Data;
 using VaccinationCard.Infrastructure.Repositories;
