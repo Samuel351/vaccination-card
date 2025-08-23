9525f6314b4f001572752523947649785d947b99
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-23 20:59:25 -0300

    Update Person.cs

--- Back-end/VaccinationCard/VaccinationCard.Domain/Entities/Person.cs
+++ Back-end/VaccinationCard/VaccinationCard.Domain/Entities/Person.cs
@@ -3,6 +3,16 @@
     public class Person : EntityBase
     {
         public string Name { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
         
         public List<Vaccination> Vaccinations { get; set; } = [];
 
