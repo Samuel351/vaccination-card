a76afa2d9aab09be6119b76b83fe98b4655822ad
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-24 07:08:28 -0300

    Feat: New result pattern implementation

---
+++ Back-end/VaccinationCard/VaccinationCard.Domain/Abstractions/Result.cs
@@ -0,0 +1,37 @@
+﻿namespace Domain.Abstractions;
+
+public class Result
+{
+    protected Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+
+    public static Result Failure(Error error) => new(false, error);
+}
+
+public class Result<T> : Result
+{
+    private Result(T value) : base(true, Error.None)
+    {
+        Value = value;
+    }
+
+    private Result(Error error) : base(false, error) { }
+
+    public T? Value { get; }
+
+    public static Result<T> Success(T value) => new(value);
+
+    public static new Result<T> Failure(Error error) => new(error);
+}
+
