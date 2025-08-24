2b8ad07e46b7b22c7e7c6a2895ef2cbbe6645445
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-23 22:06:06 -0300

    Feat: First fluent validation implementation

--- Back-end/VaccinationCard/VaccinationCard.Application/DependencyInjection.cs
+++ Back-end/VaccinationCard/VaccinationCard.Application/DependencyInjection.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaccinationCard.Application.Behaviors;
 using VaccinationCard.Application.Interfaces.Repositories;
 using VaccinationCard.Application.Persons.Commands.CreatePerson;
 
@@ -14,6 +17,8 @@ namespace VaccinationCard.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreatePersonCommandHandler>());
+            services.AddValidatorsFromAssembly(typeof(CreatePersonCommandHandler).Assembly, includeInternalTypes: true);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
             return services;
         }
---
+++ Back-end/VaccinationCard/VaccinationCard.Application/Persons/Validators/CreatePersonValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using VaccinationCard.Application.Persons.Commands.CreatePerson;
+
+namespace VaccinationCard.Application.Persons.Validators
+{
+    internal class CreatePersonValidator : AbstractValidator<CreatePersonCommand>
+    {
+        public CreatePersonValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("O nome da pessoa é obrigatório")
+                .MinimumLength(3).WithMessage("O nome da pessoa ter no mínimo 3 caracteres.");
+
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("Email deve ser válido");
+        }
+    }
+}
