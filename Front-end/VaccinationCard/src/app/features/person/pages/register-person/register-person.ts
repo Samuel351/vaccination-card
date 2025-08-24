48839ca7aa0de7e2c0823da7d29db165feb92172
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-23 22:41:10 -0300

    Feat: Modal component

--- Front-end/VaccinationCard/src/app/features/person/pages/register-person/register-person.ts
+++ Front-end/VaccinationCard/src/app/features/person/pages/register-person/register-person.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../../../shared/components/input-component/input-component';
 import { ButtonComponent } from "../../../../shared/components/button-component/button-component";
+import { ModalComponent } from "../../../../shared/components/modal-component/modal-component";
 
 @Component({
   selector: 'app-register-person',
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, ModalComponent],
   standalone: true,
   templateUrl: './register-person.html',
   styleUrl: './register-person.scss'
