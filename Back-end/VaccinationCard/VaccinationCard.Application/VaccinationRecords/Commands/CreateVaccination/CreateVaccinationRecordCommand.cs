038a39bfe58f3715aa66999c95dbb688757cf234
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-23 21:18:02 -0300

    Refactor: Adding folder for better organization

--- Back-end/VaccinationCard/VaccinationCard.Application/VaccinationRecords/Commands/CreateVaccination/CreateVaccinationRecordCommand.cs
+++ Back-end/VaccinationCard/VaccinationCard.Application/VaccinationRecords/Commands/CreateVaccination/CreateVaccinationRecordCommand.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
 using VaccinationCard.SharedKernel;
 
-namespace VaccinationCard.Application.VaccinationRecords.Commands
+namespace VaccinationCard.Application.VaccinationRecords.Commands.CreateVaccination
 {
     public sealed record CreateVaccinationRecordCommand(Guid PersonId, Guid VaccineId, int DoseNumber, DateTime VaccinationDate) : IRequest<Result<bool>>;
 }
