966623aacbf43246cd438781c513b885b74c6d5c
Samuel <sscosta2003@outlook.com>
2025-08-27 13:10:22 -0300

    Feat: Adding more tests cases to vaccination suit test
    ValidateNewDose_ShouldReturnFailure_WhenApplicationDateIsInTheFuture
    
    ValidateNewDose_ShouldReturnFailure_WhenApplicationDateIsOlderThanPreviousApplicationDates
    
    ValidateNewDose_ShouldReturnSucess_WhenRegisteringVaccinationInValidDate

--- Back-end/VaccinationCard/VaccinationCard.Tests/Vaccinations/VaccinationServiceTests.cs
+++ Back-end/VaccinationCard/VaccinationCard.Tests/Vaccinations/VaccinationServiceTests.cs
@@ -25,7 +25,6 @@ namespace VaccinationCard.Tests.Vaccinations
         [Fact]
         public async Task ValidateNewDose_ShouldReturnFailure_WhenDoseAlreadyApplied()
         {
-
             // Arrange
             var vaccine = new Vaccine("COVID-19", 3);
 
@@ -101,6 +100,84 @@ namespace VaccinationCard.Tests.Vaccinations
             Assert.True(result.IsFailure);
             Assert.Equal(VaccinationErrors.DoseNotAppliedYet(3), result.Error);
 
+        }
+
+        [Fact]
+        public async Task ValidateNewDose_ShouldReturnFailure_WhenApplicationDateIsInTheFuture()
+        {
+            // Arrange
+            // Vaccine requires four doses
+            var vaccine = new Vaccine("COVID-19", 4);
+
+            var vaccination = new Vaccination(VaccineId, PersonId, 3, DateTime.Now.AddDays(3));
+
+            var savedVaccinations = new List<Vaccination>()
+            {
+                new(VaccineId, PersonId, 1, DateTime.Now.AddDays(-90)),
+                new(VaccineId, PersonId, 2, DateTime.Now.AddDays(-60)),
+            };
+
+            _vaccinationRepositoryMock.Setup(repo => repo.GetVaccinations(vaccination.VaccineId, vaccination.PersonId))
+                .ReturnsAsync(savedVaccinations);
+
+            // Act
+            var result = await _vaccinationService.ValidateNewDose(vaccination, vaccine);
+
+            // Assert
+            Assert.True(result.IsFailure);
+            Assert.Equal(VaccinationErrors.DoseApplicationDateInFuture, result.Error);
+        }
+
+
+        [Fact]
+        public async Task ValidateNewDose_ShouldReturnFailure_WhenApplicationDateIsOlderThanPreviousApplicationDates()
+        {
+            // Arrange
+            // Vaccine requires four doses
+            var vaccine = new Vaccine("COVID-19", 4);
+
+            var vaccination = new Vaccination(VaccineId, PersonId, 3, DateTime.Now.AddDays(-90));
+
+            var savedVaccinations = new List<Vaccination>()
+            {
+                new(VaccineId, PersonId, 1, DateTime.Now.AddDays(-90)),
+                new(VaccineId, PersonId, 2, DateTime.Now.AddDays(-60)),
+            };
+
+            _vaccinationRepositoryMock.Setup(repo => repo.GetVaccinations(vaccination.VaccineId, vaccination.PersonId))
+                .ReturnsAsync(savedVaccinations);
+
+            // Act
+            var result = await _vaccinationService.ValidateNewDose(vaccination, vaccine);
+
+            // Assert
+            Assert.True(result.IsFailure);
+            Assert.Equal(VaccinationErrors.DoseApplicationDate, result.Error);
+        }
+
+        [Fact]
+        public async Task ValidateNewDose_ShouldReturnSucess_WhenRegisteringVaccinationInValidDate()
+        {
+            // Arrange
+            // Vaccine requires four doses
+            var vaccine = new Vaccine("COVID-19", 4);
+
+            var vaccination = new Vaccination(VaccineId, PersonId, 3, DateTime.Now.AddDays(-30));
+
+            var savedVaccinations = new List<Vaccination>()
+            {
+                new(VaccineId, PersonId, 1, DateTime.Now.AddDays(-90)),
+                new(VaccineId, PersonId, 2, DateTime.Now.AddDays(-60)),
+            };
+
+            _vaccinationRepositoryMock.Setup(repo => repo.GetVaccinations(vaccination.VaccineId, vaccination.PersonId))
+                .ReturnsAsync(savedVaccinations);
+
+            // Act
+            var result = await _vaccinationService.ValidateNewDose(vaccination, vaccine);
+
+            // Assert
+            Assert.True(result.IsSuccess);
         }
     }
 }
