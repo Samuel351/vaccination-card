a8bb0f169da310f0455f807d5558a20fca49a63b
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2025-08-27 20:42:23 -0300

    Fix: When token was invalid the app was not redirecting to login

--- Front-end/VaccinationCard/src/app/features/authentication/interceptors/tokenInterceptor.ts
+++ Front-end/VaccinationCard/src/app/features/authentication/interceptors/tokenInterceptor.ts
@@ -1,20 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthenticationService } from '../services/authentication-service';
-import { tap } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 
 export const tokenInterceptorFn: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthenticationService);
   const token = authService.getToken();
   const cloned = token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req;
 
-  return next(cloned).pipe(
-    tap({
-      error: (err) => {
-        if (err.status === 401) {
-          authService.logout(); // Corrigido o nome do método
-        }
+return next(cloned).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
       }
+
+      if (error.status === 0 && error.statusText === 'Unknown Error') {
+        authService.logout();
+      }
+
+      return throwError(() => error);
     })
   );
 };
